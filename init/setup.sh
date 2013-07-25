c51323c2278465cba53e2f4105791a869f5ee11c
SirReal <jon.surrell@gmail.com>
2013-07-25 02:24:00 +0200

    good progress on init script

---
+++ init/setup.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# Determine current system
+case $(uname) in
+    "Linux")
+        echo "You're on a Linux system!"
+        THIS_SYSTEM=linux
+        ;;
+    "Darwin")
+        echo "You're on a Mac!"
+        THIS_SYSTEM=mac
+        ;;
+    *)
+        echo "I'm not sure what system you're on :(" >&2
+        exit 1
+        ;;
+esac
+
+
+# Test whether a command exists
+# $1 - cmd to test
+type_exists() {
+    if [ `type -P $1` ]; then
+      return 0
+    fi
+    return 1
+}
+
+run_apt() {
+    sudo add-apt-repository http://dl.google.com/linux/talkplugin/deb/
+    sudo add-apt-repository ppa:chris-lea/node.js
+    sudo apt-get update
+    sudo apt-get install -y $(<apt_packages)
+}
+
+run_brew() {
+    if ! type_exists 'brew'; then
+        echo "Homebrew not installed or not found."
+        read -e -p "Install? (y/N): " HB
+        HB=${HB:-"NO"}
+        case $inst in
+            "y"|"Y"|"yes")
+                ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+                ;;
+            *)
+                return
+                ;;
+        esac
+    fi
+    if [ ! -f brew_formula ]; then
+        echo "brew_formula file not found. Aborting formula install." >&2
+        return
+    fi
+
+    brew update
+    brew install $(<brew_formula)
+
+}
+
+# npm package installation
+run_npm() {
+    # Check for npm
+    if ! type_exists 'npm'; then
+        echo "npm not installed or not found. Aborting Node.js module install." >&2
+        return
+    fi
+
+    if [ ! -f global_node_modules ]; then
+        echo "global_node_modules file not found. Aborting Node.js module install." >&2
+        return
+    fi
+
+    echo "Installing Node.js modules..."
+
+    # Install packages globally and quietly
+    sudo npm install --global --quiet $(<global_node_modules)
+
+    echo "Node.js modules installed!"
+}
+
+
+# The variable $0 is the script's name. The total number of arguments is stored in $#. The variables $@ and $* return all the arguments.
+if [[ $# -eq 0 ]]; then
+    echo "Full dotfile bootstrap..."
+elif [[ $# -gt 0 ]]; then
+    for var in "$@"; do
+        case "$var" in
+            "bootstrap")
+                echo "Full system bootstrap. This will symlink files to ~ (possibly overwriting)."
+                read -e -p "Continue? (y/N): " BS
+                BS=${BS:-"NO"}
+                case $BS in
+                    "y"|"Y"|"yes")
+                        # Do everything
+                        echo "bootstrap..."
+                        ;;
+                    *)
+                        exit 0
+                        ;;
+                esac
+                ;;
+            "packages" | "modules")
+                echo "Install packages..."
+                ;;
+            "node" | "npm")
+                run_npm
+                ;;
+            "brew"|"homebrew")
+                run_brew
+                ;;
+            "ruby" | "gem")
+                echo "Just ruby"
+                ;;
+            *)
+                echo "I didn't understand \"$var\" :("
+                ;;
+        esac
+    done
+fi
