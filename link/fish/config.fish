631d2dfce574ee5dfb56bb560fc78a8fd4c96398
SirReal <jon.surrell@gmail.com>
2014-11-01 07:04:42 +0100

    fish $PATH settings

--- link/fish/config.fish
+++ link/fish/config.fish
@@ -1,1 +1,15 @@
+## Path
+if test -d $HOME/go/bin
+  set -gx PATH $HOME/go/bin $PATH
+end
+
+if test -d $HOME/.cabal/bin
+  set -gx PATH $HOME/.cabal/bin $PATH
+end
+
+if test -d $HOME/bin
+  set -gx PATH $HOME/bin $PATH
+end
+
+## Start session in tmux
 tmux
