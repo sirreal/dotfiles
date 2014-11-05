f2d81f656ee16f02a3097289ef5dd25e9c739f96
sirreal <jon.surrell@gmail.com>
2014-11-05 14:50:12 +0100

    fich functions

--- link/fish/functions/allupdate.fish
+++ link/fish/functions/allupdate.fish
@@ -1,11 +1,11 @@
 function allupdate --description 'System update'
 	begin
 		if	begin
-			which apt-get > /dev/null
-			and test -x (which apt-get)
-		end
+				which apt-get > /dev/null
+				and test -x (which apt-get)
+			end
 			sudo apt-get update -qq
-			sudo apt-get upgrade -y -qq
+			sudo apt-get upgrade -yqqu
 		end
 		if	begin
 				which brew > /dev/null
@@ -15,12 +15,18 @@ function allupdate --description 'System update'
 			brew upgrade
 		end
 		if	begin
-			which npm > /dev/null
-			and test -x (which npm)
-		end
-			for p in (npm -g outdated --parseable --depth=0 | cut -d: -f2)
-				npm -g i "$p"
+				which npm > /dev/null
+				and test -x (which npm)
 			end
+			if test -w (npm config get prefix --global)/lib
+				function _allupdate_CMD; npm $argv; end
+			else
+				function _allupdate_CMD; sudo npm $argv; end
+			end
+			for p in (npm -g outdated --parseable --depth=0 | cut -d: -f2)
+				_allupdate_CMD -g i "$p"
+			end
+			functions -e _allupdate_CMD
 		end
 	end
 end
--- link/fish/functions/fish_greeting.fish
+++ link/fish/functions/fish_greeting.fish
@@ -1,5 +1,8 @@
 function fish_greeting --description 'Print message at startup'
-	if not test -z $TMUX
-		fortune -a
+	if	begin
+			which fortune > /dev/null
+			and not test -z $TMUX
+		end
+		fortune -as
 	end
 end
