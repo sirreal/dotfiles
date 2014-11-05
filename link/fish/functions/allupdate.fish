b5b9c25f34506bd56106b6135bbf9146cf199e1c
sirreal <jon.surrell@gmail.com>
2014-11-05 10:42:44 +0100

    fish allupdate

--- link/fish/functions/allupdate.fish
+++ link/fish/functions/allupdate.fish
@@ -1,21 +1,26 @@
 function allupdate --description 'System update'
-	if test -x (which apt-get)
-		and test -x (which apt-get)
-		echo "apt found"
-		sudo apt-get update -qq
-		sudo apt-get upgrade -y -q
-	end
-	if test (which brew)
-		and test -x (which brew)
-		echo "brew found"
-		brew update
-		brew upgrade
-	end
-	if test (which npm)
-		and test -x (which npm)
-		echo "npm found"
-		for p in (npm -g outdated --parseable --depth=0 | cut -d: -f2)
-			npm -g i "$p"
+	begin
+		if	begin
+			which apt-get > /dev/null
+			and test -x (which apt-get)
+		end
+			sudo apt-get update -qq
+			sudo apt-get upgrade -y -qq
+		end
+		if	begin
+				which brew > /dev/null
+				and test -x (which brew)
+			end
+			brew update
+			brew upgrade
+		end
+		if	begin
+			which npm > /dev/null
+			and test -x (which npm)
+		end
+			for p in (npm -g outdated --parseable --depth=0 | cut -d: -f2)
+				npm -g i "$p"
+			end
 		end
 	end
 end
