0181bf68e6041a1f9bb6d375bc57078b0fa6100a
Jon Surrell <sirreal@users.noreply.github.com>
2021-09-02 22:32:26 +0200

    Lua nvim config cleanup

--- link/.config/nvim/lua/config/null-ls.lua
+++ link/.config/nvim/lua/config/null-ls.lua
@@ -1,14 +1,5 @@
 local null_ls = require("null-ls")
-local root_pattern = require("lspconfig/util").root_pattern
 
-function root_dir(bufnr)
-	local fname = vim.api.nvim_buf_get_name(bufnr)
-	return root_pattern("package.json")(fname) or root_pattern(".git")(fname)
-end
-
-function require_node_bin(bufnr) end
-
-local eslint = null_ls.builtins.diagnostics.eslint
 null_ls.config({
 	debug = true,
 	sources = {
@@ -21,8 +12,5 @@ null_ls.config({
 	},
 })
 require("lspconfig")["null-ls"].setup({
-	on_attach = function(client, bufnr)
-		print(root_dir(bufnr))
-		require("config.utils").on_attach(client, bufnr)
-	end,
+	on_attach = require("config.utils").on_attach,
 })
