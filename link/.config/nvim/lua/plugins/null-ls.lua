23913ed74981f1b9c29d28032ba195ac435d86c5
Jon Surrell <sirreal@users.noreply.github.com>
2021-09-15 15:46:00 +0200

    Working on eslint code actions

--- link/.config/nvim/lua/plugins/null-ls.lua
+++ link/.config/nvim/lua/plugins/null-ls.lua
@@ -9,46 +9,168 @@ end
 local tsserver_fts = require("lspconfig").tsserver.filetypes
 local helpers = require("null-ls.helpers")
 
-local code_action_handler = function(params)
-	local row = params.row
-	local indentation = params.content[row]:match("^%s+") or ""
+local get_offset_positions = function(content, start_offset, end_offset)
+	-- ESLint uses character offsets, so convert to byte indexes to handle multibyte characters
+	local to_string = table.concat(content, "\n")
+	start_offset = vim.str_byteindex(to_string, start_offset + 1)
+	end_offset = vim.str_byteindex(to_string, end_offset + 1)
+	-- save original window position and virtualedit setting
+	local view = vim.fn.winsaveview()
+	local virtualedit = vim.opt.virtualedit
+	vim.opt.virtualedit = "all"
+	vim.cmd("go " .. start_offset)
+	-- (1,0)-indexed
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local col = cursor[2] + 1
+	vim.cmd("go " .. end_offset)
+	cursor = vim.api.nvim_win_get_cursor(0)
+	local end_row, end_col = cursor[1], cursor[2] + 1
+	-- restore state
+	vim.fn.winrestview(view)
+	vim.opt.virtualedit = virtualedit
+	return col, end_col, end_row
+end
 
-	local rules, actions = {}, {}
-	for _, message in ipairs(params.messages) do
-		log.debug("")
-		log.debug(message)
-		if is_fixable(message, row) then
-			log.debug("fixable")
-			if message.suggestions then
-				for _, suggestion in ipairs(message.suggestions) do
-					table.insert(actions, generate_suggestion_action(suggestion, message, params))
-				end
-			end
+local function read_line(line, bufnr)
+	return vim.api.nvim_buf_get_lines(bufnr, line, line + 1, false)[1]
+end
 
-			if message.fix then
-				table.insert(actions, generate_fix_action(message, params))
-			end
-		end
-		log.debug(rules)
-		if message.ruleId and not rules[message.ruleId] then
-			log.debug("gen disable")
-			rules[message.ruleId] = true
+local function get_fix_range(problem, params)
+	-- 1-indexed
+	local row = problem.line
+	local offset = problem.fix.range[1]
+	local end_offset = problem.fix.range[2]
+	local col, end_col, end_row = get_offset_positions(params.content, offset, end_offset)
+	return { row = row, col = col, end_row = end_row, end_col = end_col }
+end
+
+local function generate_edit_action(title, new_text, range, params)
+	return {
+		title = title,
+		action = function()
+			-- 0-indexed
+			vim.api.nvim_buf_set_text(
+				params.bufnr,
+				range.row - 1,
+				range.col - 1,
+				range.end_row - 1,
+				range.end_col - 1,
+				vim.split(new_text, "\n")
+			)
+		end,
+	}
+end
+
+local function generate_edit_line_action(title, new_text, row, params)
+	return {
+		title = title,
+		action = function()
+			-- 0-indexed
+			vim.api.nvim_buf_set_lines(params.bufnr, row - 1, row - 1, false, { new_text })
+		end,
+	}
+end
+
+local range_includes_row = function(message, row)
+	if not message or not message.line then
+		return false
+	end
+
+	if message.endline then
+		return message.line <= row and message.endLine >= row
+	end
+
+	return message.line == row
+end
+
+local generate_suggestion_action = function(suggestion, message, params)
+	local title = suggestion.desc
+	local new_text = suggestion.fix.text
+	local range = get_message_range(message)
+	return generate_edit_action(title, new_text, range, params)
+end
+
+local generate_fix_actions = function(message, indentation, params)
+	if not message.fix then
+		return {}
+	end
+	local title = "Apply suggested fix for ESLint rule " .. message.ruleId
+	local new_text = message.fix.text
+	local range = get_fix_range(message, params)
+	return { generate_edit_action(title, new_text, range, params) }
+end
+
+local generate_disable_actions = function(message, indentation, params)
+	local rule_id = message.ruleId
+
+	if not rule_id then
+		return {}
+	end
+
+	local actions = {}
+
+	--
+	-- Disable for line
+	--
+	local line_title = "ESLint: Disable " .. rule_id .. " for line"
+	-- TODO: check for existing disable and add
+	local previous_line = read_line(message.line - 2) -- buffer is 0-indexed, eslint message 1-indexed
+	log.debug(previous_line)
+	local line_new_text = indentation .. "// eslint-disable-next-line " .. rule_id
+	table.insert(actions, generate_edit_line_action(line_title, line_new_text, message.line, params))
+
+	--
+	-- Disable entire file
+	--
+	local file_title = "ESLint: Disable " .. rule_id .. " for the entire file"
+	-- TODO: check for existing disable and add
+	local first_line = read_line(1)
+	log.debug(first_line)
+	local file_new_text = "/* eslint-disable " .. rule_id .. " */"
+	table.insert(actions, generate_edit_line_action(file_title, file_new_text, 1, params))
+
+	return actions
+end
+
+local function generate_actions(messages, indentation, params)
+	local actions = {}
+
+	local observed_rules = {}
+	for _, message in ipairs(messages) do
+		vim.list_extend(actions, generate_fix_actions(message, indentation, params))
+		if message.ruleId and not observed_rules[message.ruleId] then
 			vim.list_extend(actions, generate_disable_actions(message, indentation, params))
-			log.debug(actions)
-		else
-			log.debug("no gen disable")
+			observed_rules[message.ruleId] = true
 		end
 	end
 
 	return actions
 end
 
+local code_action_handler = function(params)
+	local row = params.row
+	local indentation = params.content[row]:match("^%s+") or ""
+
+	local problems_in_range = {}
+	for _, problem in ipairs(params.messages) do
+		if range_includes_row(problem, row) then
+			table.insert(problems_in_range, problem)
+		end
+	end
+
+	return generate_actions(problems_in_range, indentation, params)
+end
+
 local on_output = function(params)
 	params.messages = params.output and params.output[1] and params.output[1].messages or {}
 
-	log.debug(params.messages)
-	return {}
-	-- return code_action_handler(params)
+	local ok, result = pcall(code_action_handler, params)
+	if not ok then
+		log.error(result)
+		return {}
+	else
+		return result
+	end
 end
 
 -- From https://github.com/jose-elias-alvarez/null-ls.nvim/blob/da8bb757c630b96fd26030df56fd3a070fbf16a1/lua/null-ls/builtins/diagnostics.lua#L224-L241
@@ -81,7 +203,7 @@ local eslint_diagnostics_source = {
 		check_exit_code = function(code)
 			return code <= 1
 		end,
-		use_cache = true,
+		use_cache = false,
 		on_output = handle_eslint_output,
 	}),
 }
@@ -98,7 +220,7 @@ local eslint_code_action_source = {
 		check_exit_code = function(code)
 			return code <= 1
 		end,
-		use_cache = true,
+		use_cache = false,
 		on_output = on_output,
 	}),
 }
@@ -143,32 +265,6 @@ require("lspconfig")["null-ls"].setup({
 -- 	end
 -- end
 
--- local get_offset_positions = function(content, start_offset, end_offset)
--- 	-- ESLint uses character offsets, so convert to byte indexes to handle multibyte characters
--- 	local to_string = table.concat(content, "\n")
--- 	start_offset = vim.str_byteindex(to_string, start_offset + 1)
--- 	end_offset = vim.str_byteindex(to_string, end_offset + 1)
-
--- 	-- save original window position and virtualedit setting
--- 	local view = vim.fn.winsaveview()
--- 	local virtualedit = vim.opt.virtualedit
--- 	vim.opt.virtualedit = "all"
-
--- 	vim.cmd("go " .. start_offset)
--- 	-- (1,0)-indexed
--- 	local cursor = api.nvim_win_get_cursor(0)
--- 	local col = cursor[2] + 1
--- 	vim.cmd("go " .. end_offset)
--- 	cursor = api.nvim_win_get_cursor(0)
--- 	local end_row, end_col = cursor[1], cursor[2] + 1
-
--- 	-- restore state
--- 	vim.fn.winrestview(view)
--- 	vim.opt.virtualedit = virtualedit
-
--- 	return col, end_col, end_row
--- end
-
 -- local is_fixable = function(problem, row)
 -- 	if not problem or not problem.line then
 -- 		return false
@@ -193,43 +289,6 @@ require("lspconfig")["null-ls"].setup({
 -- 	local end_col = problem.endColumn or 1
 
 -- 	return { row = row, col = col, end_row = end_row, end_col = end_col }
--- end
-
--- local get_fix_range = function(problem, params)
--- 	-- 1-indexed
--- 	local row = problem.line
--- 	local offset = problem.fix.range[1]
--- 	local end_offset = problem.fix.range[2]
--- 	local col, end_col, end_row = get_offset_positions(params.content, offset, end_offset)
-
--- 	return { row = row, col = col, end_row = end_row, end_col = end_col }
--- end
-
--- local generate_edit_action = function(title, new_text, range, params)
--- 	return {
--- 		title = title,
--- 		action = function()
--- 			-- 0-indexed
--- 			api.nvim_buf_set_text(
--- 				params.bufnr,
--- 				range.row - 1,
--- 				range.col - 1,
--- 				range.end_row - 1,
--- 				range.end_col - 1,
--- 				vim.split(new_text, "\n")
--- 			)
--- 		end,
--- 	}
--- end
-
--- local generate_edit_line_action = function(title, new_text, row, params)
--- 	return {
--- 		title = title,
--- 		action = function()
--- 			-- 0-indexed
--- 			api.nvim_buf_set_lines(params.bufnr, row - 1, row - 1, false, { new_text })
--- 		end,
--- 	}
 -- end
 
 -- local generate_suggestion_action = function(suggestion, message, params)
