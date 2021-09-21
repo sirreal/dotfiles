d4bedcf86c0b80ae9ddece50ae62862b97bd8494
Jon Surrell <sirreal@users.noreply.github.com>
2021-09-21 22:43:22 +0200

    better completions

--- link/.config/nvim/lua/plugins/cmp.lua
+++ link/.config/nvim/lua/plugins/cmp.lua
@@ -1,11 +1,13 @@
-vim.o.completeopt = "menuone,noselect"
+vim.o.completeopt = "menu,menuone,noselect"
 
 local cmp = require("cmp")
 
-require("cmp").setup({
+cmp.setup({
 	mapping = {
 		["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = function(fallback)
 			if vim.fn.pumvisible() == 1 then
 				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
