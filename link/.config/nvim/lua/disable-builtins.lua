ef34af3378c762fd3a567dec1f49d44e9f3a9923
Jon Surrell <sirreal@users.noreply.github.com>
2021-09-23 15:35:45 +0200

    Disable some unused providers

--- link/.config/nvim/lua/disable-builtins.lua
+++ link/.config/nvim/lua/disable-builtins.lua
@@ -1,3 +1,7 @@
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python_provider = 0 -- Python2
+vim.g.loaded_ruby_provider = 0
+
 vim.g.loaded_gzip = 1
 vim.g.loaded_zip = 1
 vim.g.loaded_zipPlugin = 1
