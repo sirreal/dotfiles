9a4cd2334b875312aff25efa7f51f0e4c36b74a3
Jon Surrell <sirreal@users.noreply.github.com>
2021-10-05 18:59:47 +0200

    playing with luasnip

--- link/.config/nvim/lua/plugins/cmp.lua
+++ link/.config/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,7 @@
 vim.o.completeopt = "menu,menuone,noselect"
 
 local cmp = require("cmp")
+require("plugins.luasnip")
 
 cmp.setup({
 	mapping = {
@@ -33,3 +34,11 @@ cmp.setup({
 		{ name = "luasnip" },
 	},
 })
+
+vim.cmd([[
+  imap <silent><expr> <c-k> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<c-k>'
+  inoremap <silent> <c-j> <cmd>lua require('luasnip').jump(-1)<CR>
+  imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
+  snoremap <silent> <c-k> <cmd>lua require('luasnip').jump(1)<CR>
+  snoremap <silent> <c-j> <cmd>lua require('luasnip').jump(-1)<CR>
+]])
