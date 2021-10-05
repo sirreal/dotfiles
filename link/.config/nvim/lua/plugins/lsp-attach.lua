0426625fa6540bff439681c05aaa76659c9a8181
Jon Surrell <sirreal@users.noreply.github.com>
2021-10-05 19:10:22 +0200

    sort lsp commands

--- link/.config/nvim/lua/plugins/lsp-attach.lua
+++ link/.config/nvim/lua/plugins/lsp-attach.lua
@@ -9,30 +9,36 @@ return function(client, bufnr)
 
 	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	vim.cmd("command! LspDefinition lua vim.lsp.buf.definition()")
-	vim.cmd("command! LspDeclaration lua vim.lsp.buf.declaration()")
-	vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting_sync()")
-	vim.cmd("command! LspTypeDefinition lua vim.lsp.buf.type_definition()")
-	vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+	--
+	-- core lsp
+	--
 	vim.cmd("command! LspCodeAction lua vim.lsp.buf.code_action()")
 	vim.cmd("command! LspCodeActionRange lua vim.lsp.buf.range_code_action()")
-	-- vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
-	-- vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
-	-- vim.cmd("command! LspReferences lua vim.lsp.buf.references()")
-	-- vim.cmd("command! LspDianosticPrev lua vim.lsp.diagnostic.goto_prev()")
-	-- vim.cmd("command! LspDianosticNext lua vim.lsp.diagnostic.goto_next()")
+	vim.cmd("command! LspDeclaration lua vim.lsp.buf.declaration()")
+	vim.cmd("command! LspDefinition lua vim.lsp.buf.definition()")
 	-- vim.cmd("command! LspDianosticLine lua vim.lsp.diagnostic.show_line_diagnostics()")
+	-- vim.cmd("command! LspDianosticNext lua vim.lsp.diagnostic.goto_next()")
+	-- vim.cmd("command! LspDianosticPrev lua vim.lsp.diagnostic.goto_prev()")
+	vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting_sync()")
+	-- vim.cmd("command! LspHover lua vim.lsp.buf.hover()")
+	vim.cmd("command! LspImplementation lua vim.lsp.buf.implementation()")
+	-- vim.cmd("command! LspReferences lua vim.lsp.buf.references()")
+	-- vim.cmd("command! LspRename lua vim.lsp.buf.rename()")
 	-- vim.cmd("command! LspSignatureHelp lua vim.lsp.buf.signature_help()")
+	vim.cmd("command! LspTypeDefinition lua vim.lsp.buf.type_definition()")
 
+	--
+	-- lspsaga variants
+	--
 	-- vim.cmd("command! LspCodeAction lua require('lspsaga.codeaction').code_action()")
 	-- vim.cmd("command! LspCodeActionRange lua require('lspsaga.codeaction').range_code_action()")
-	vim.cmd("command! LspHover lua require('lspsaga.hover').render_hover_doc()")
-	vim.cmd("command! LspRename lua require('lspsaga.rename').rename()")
-	vim.cmd("command! LspDianosticPrev lua require('lspsaga.diagnostic').lsp_jump_diagnostic_prev()")
-	vim.cmd("command! LspDianosticNext lua require('lspsaga.diagnostic').lsp_jump_diagnostic_next()")
 	vim.cmd("command! LspDianosticLine lua require('lspsaga.diagnostic').show_line_diagnostics()")
-	vim.cmd("command! LspSignatureHelp lua require('lspsaga.signaturehelp').signature_help()")
+	vim.cmd("command! LspDianosticNext lua require('lspsaga.diagnostic').lsp_jump_diagnostic_next()")
+	vim.cmd("command! LspDianosticPrev lua require('lspsaga.diagnostic').lsp_jump_diagnostic_prev()")
+	vim.cmd("command! LspHover lua require('lspsaga.hover').render_hover_doc()")
 	vim.cmd("command! LspReferences lua require('lspsaga.provider').lsp_finder()")
+	vim.cmd("command! LspRename lua require('lspsaga.rename').rename()")
+	vim.cmd("command! LspSignatureHelp lua require('lspsaga.signaturehelp').signature_help()")
 
 	map("n", "gd", ":LspDefinition<CR>")
 	map("n", "gi", ":LspImplementation<CR>")
