fa39f812b58122c46c65b1b1e01f8f6cfb6bf751
Jon Surrell <sirreal@users.noreply.github.com>
2021-11-19 19:55:52 +0100

    Use null-ls builtin eslint stuff
    now supports code actions

--- link/.config/nvim/lua/plugins/null-ls.lua
+++ link/.config/nvim/lua/plugins/null-ls.lua
@@ -6,63 +6,6 @@ if not ok then
 	return
 end
 
-local eslint = require("plugins.eslint")
-local helpers = require("null-ls.helpers")
-
-local tsserver_filetypes
-ok, tsserver_filetypes = pcall(function()
-	return require("lspconfig").tsserver.document_config.default_config.filetypes
-end)
-if not ok then
-	log.warn("could not load tsserver filetypes")
-	tsserver_filetypes = {
-		"javascript",
-		"javascriptreact",
-		"javascript.jsx",
-		"typescript",
-		"typescriptreact",
-		"typescript.tsx",
-	}
-end
-
-local eslint_diagnostics_source = {
-	name = "jons-eslint-diagnostics",
-	method = null_ls.methods.DIAGNOSTICS,
-	filetypes = tsserver_filetypes,
-	generator = helpers.generator_factory({
-		command = "eslint_d",
-		args = { "-f", "json", "--stdin", "--stdin-filename", "$FILENAME" },
-		to_stdin = true,
-		format = "json_raw",
-		check_exit_code = function(code)
-			return code <= 1
-		end,
-		use_cache = true,
-		on_output = eslint.handle_eslint_output,
-	}),
-}
-
-local eslint_code_action_source = {
-	name = "jons-eslint-code-action",
-	method = null_ls.methods.CODE_ACTION,
-	filetypes = tsserver_filetypes,
-	generator = helpers.generator_factory({
-		command = "eslint_d",
-		args = { "-f", "json", "--stdin", "--stdin-filename", "$FILENAME" },
-		to_stdin = true,
-		format = "json_raw",
-		check_exit_code = function(code)
-			return code <= 1
-		end,
-		use_cache = true,
-		on_output = function(params)
-			local win = vim.api.nvim_get_current_win()
-			params.win = win
-			return eslint.code_action_handler(params)
-		end,
-	}),
-}
-
 null_ls.config({
 	debug = true,
 	sources = {
@@ -70,9 +13,8 @@ null_ls.config({
 		null_ls.builtins.formatting.prettier,
 		null_ls.builtins.diagnostics.stylelint,
 		null_ls.builtins.formatting.stylelint,
-		-- null_ls.builtins.diagnostics.eslint_d,
-		eslint_diagnostics_source,
-		eslint_code_action_source,
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.code_actions.eslint_d,
 	},
 })
 
