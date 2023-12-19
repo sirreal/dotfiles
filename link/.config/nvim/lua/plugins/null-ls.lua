3824c2e13d8942fc0c3ed68491012cd11954b045
Jon Surrell <sirreal@users.noreply.github.com>
2023-12-19 10:42:04 +0100

    Switch back to prettier formatting

--- link/.config/nvim/lua/plugins/null-ls.lua
+++ link/.config/nvim/lua/plugins/null-ls.lua
@@ -3,17 +3,17 @@ local null_ls = require("null-ls")
 null_ls.setup({
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		-- null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.dprint.with({
-			extra_filetypes = {
-				"css",
-				"dockerfile",
-				"json5",
-				"jsonc",
-				"sass",
-				"scss",
-			},
-		}),
+		null_ls.builtins.formatting.prettier,
+		-- null_ls.builtins.formatting.dprint.with({
+		-- 	extra_filetypes = {
+		-- 		"css",
+		-- 		"dockerfile",
+		-- 		"json5",
+		-- 		"jsonc",
+		-- 		"sass",
+		-- 		"scss",
+		-- 	},
+		-- }),
 		null_ls.builtins.diagnostics.stylelint,
 		-- null_ls.builtins.formatting.stylelint,
 		null_ls.builtins.diagnostics.eslint_d,
