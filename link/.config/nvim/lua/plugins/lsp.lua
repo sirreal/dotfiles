ec996890bc492fac7533a7c1ba6d3563b4d9dfdb
Jon Surrell <sirreal@users.noreply.github.com>
2025-06-05 14:12:28 +0200

    Disable LSP full line highlights

--- link/.config/nvim/lua/plugins/lsp.lua
+++ link/.config/nvim/lua/plugins/lsp.lua
@@ -19,12 +19,12 @@ vim.diagnostic.config({
 			[vim.diagnostic.severity.HINT] = "󰋼",
 			[vim.diagnostic.severity.INFO] = "󰋼",
 		},
-		linehl = {
-			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
-			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
-			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
-			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
-		},
+		-- linehl = {
+		-- 	[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+		-- 	[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+		-- 	[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+		-- 	[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+		-- },
 		numhl = {
 			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
 			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
